7e3b1deb6f793377478ce09e53529b010a32b399
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2020-12-02 21:00:58 +0000

    Add files via upload

---
+++ week10/week10/myscript.js
@@ -0,0 +1,11 @@
+﻿username = prompt("Please enter your name");
+message = "Hello, " + username + ". Your name has ";
+nameLen = username.length;
+if (nameLen > 5)
+    message = message + "more than ";
+else if (nameLen < 5)
+    message = message + "less than ";
+else
+    message = message + "exactly ";
+message = message + "5 characters.";
+alert(message);
